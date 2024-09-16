7395c556d656e4f0941d5ec18d72705906e34972
LordBasw <126990553+LordBasw@users.noreply.github.com>
2024-09-16 19:17:02 +0200

    Added NewsStories Datastore Type

---
+++ Assets/ScriptableObjects/NewsStories/NS_Template.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewsStory", menuName = "ScriptableObjects/NewsStory", order = 1)]
+public class NS_Template : ScriptableObject
+{
+
+    [SerializeField] private int money;
+    [SerializeField] private int climateChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
