c86ae84b3593481115c8d50e6a44cdcc70644189
LordBasw <126990553+LordBasw@users.noreply.github.com>
2024-10-30 20:54:01 +0100

    Added state machine to fmv player, added idle videos

--- Assets/Scripts/FMVPlayer.cs
+++ Assets/Scripts/FMVPlayer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.Events;
 using System;
+using UnityEditor.Rendering;
 
 public class FMVPlayer : NewsStoryManager
 {
@@ -23,52 +24,115 @@ public class FMVPlayer : NewsStoryManager
     [SerializeField]
     private int negativeDelay;
 
+    [SerializeField]
+    private List<VideoClip> idleVideos;
+
+    private Animator animator;
+
+    private VideoClip currentIdleVideo;
+
+    private bool isBroadcasting = false;
+
 
     private void Awake()
     {
+        //get component references
         videoPlayer = GetComponent<VideoPlayer>();
+        animator = GetComponent<Animator>();
+        //set the audio settings 
         videoPlayer.controlledAudioTrackCount = 1;
         videoPlayer.EnableAudioTrack(0, true);
         videoPlayer.SetTargetAudioSource(0, audioSource);
 
-        //trendManager = gameManagerType.Objects[0].GetComponent<TrendManager>();
-        gameObject.SetActive(true);
+        //subscribe to the external event to trigger new news stories
         EventManager.AddListener<NSStatsSentEvent>(OnNewsstoryReceived);
+        //link the looppointreached videoplayer event to OnVideoFinished
+        videoPlayer.loopPointReached += OnVideoFinished;
     }
     private void Start()
     {
+        //spawn the initial 2 news stories (remove this when tutorial is in)
         GetNewsStoryEvent getNews = Events.GetNewsStoryEvent;
         EventManager.Broadcast(getNews);
+        //Start playing idle videos
+        PlayIdle();
     }
     public void OnNewsstoryReceived(NSStatsSentEvent evt)
     {
+        //get the NS_TEMPLATE scriptableobject from the event
         this.news = evt.template;
-        Debug.Log("Running FMV Script");
+        //get the videoclip assigned to the scriptableobject
         VideoClip video = GetContent(news => news.fmv);
-        StartCoroutine(DelayedPlayNews(video));
+        //set the state to broadcast mode
+        animator.SetBool("IsBroadcasting", true);
+        isBroadcasting = true;
+
+        //start playing the news story
+        PlayNews(video);
+    }
+
+    private void PlayIdle()
+    {
+        //make sure there's actual videos loaded in (null check)
+        if (idleVideos == null || idleVideos.Count == 0)
+        {
+            Debug.LogError("no idle videos");
+            return;
+        }
+
+
+        //set the state to not broadcasting
+        animator.SetBool("IsBroadcasting", false);
+        isBroadcasting = false;
+
+        //set the current idle video to a random one picked from the list
+        currentIdleVideo = idleVideos[UnityEngine.Random.Range(0, idleVideos.Count)];
+
+        //assign that idle video to the videoplayer
+        videoPlayer.clip = currentIdleVideo;
+        //make sure it isnt looping
+        videoPlayer.isLooping = false;
+        //play the video
+        videoPlayer.Play();
     }
 
     private void PlayNews(VideoClip video)
     {
-
-        if (!gameObject.activeInHierarchy)
-        {
-            Debug.LogError("FMVPlayer is not active when trying to play news.");
-            return;
-        }
+        //set video player's video to video assigned 
         videoPlayer.clip = video;
+        //make sure it isnt looping
+        videoPlayer.isLooping = false;
+        //play the video
         videoPlayer.Play();
+        //start checking for when to drop the new news stories
         StartCoroutine(CheckVideoTime());
     }
 
+    private void OnVideoFinished(VideoPlayer source)
+    {
+        //when one idle video is finished playing, start playing a new one, make sure you arent in broadcast mode so you dont interrupt the news story
+        if (isBroadcasting)
+        {
+            animator.SetBool("IsBroadcasting", false);
+            isBroadcasting = false;
+            PlayIdle();
+        } else
+        {
+            PlayIdle();
+        }
+    }
     private IEnumerator CheckVideoTime()
     {
+        //make sure video is actually playing
         while (!videoPlayer.isPlaying)
         {
             yield return null;
         }
+
+        //take video length and remove the offset (how many seconds before the video you want the news stories to drop)
         double spawnTime = videoPlayer.length - negativeDelay;
 
+        //spawn news stories when the specefied time before the news story's end is reached
         while (videoPlayer.isPlaying)
         {
             if (videoPlayer.time >= spawnTime)
@@ -81,9 +145,11 @@ public class FMVPlayer : NewsStoryManager
         }
     }
 
-    IEnumerator DelayedPlayNews(VideoClip video)
+
+    private void OnDestroy()
     {
-        yield return new WaitForSeconds(0.3f); // Small delay to ensure everything is active
-        PlayNews(video);
+        //remove listeners when not used
+        EventManager.RemoveListener<NSStatsSentEvent>(OnNewsstoryReceived);
+        videoPlayer.loopPointReached -= OnVideoFinished;
     }
 }
